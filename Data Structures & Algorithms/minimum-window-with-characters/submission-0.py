fd453e6daf8a7df3a0ff8f1a35c8924cccf2fe65
aparna natarajan <aparnalovestocode@gmail.com>
2026-04-08 19:45:36 -0400

    Add: minimum-window-with-characters - submission-0

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-0.py
@@ -0,0 +1,33 @@
+class Solution:
+    def containsAll(self, s: str, t: str) -> bool:
+        includes = 0
+        for c in t:
+            if c in s:
+                includes += 1
+                s = s[0:s.index(c)] + s[s.index(c)+1:] 
+        return includes == len(t)
+            
+    def minWindow(self, s: str, t: str) -> str:
+        locations = {}
+        start = 0
+        end = 0
+
+        result = None
+
+        while start < len(s) and end < len(s):
+            if s[start] not in t:
+                start += 1
+            if not self.containsAll(s[start:end + 1], t):
+                end += 1
+            else:
+                print(s[start:end+1])
+                if result == None or len(s[start:end+1]) < len(result):
+                    result = s[start:end + 1]
+                start += 1
+        
+        if result == None:
+            return ""
+
+        return result
+                
+        
