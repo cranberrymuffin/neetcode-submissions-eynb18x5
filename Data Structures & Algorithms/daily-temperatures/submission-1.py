92c65b126c7df92927a912db2f46a633512d5cc9
aparna natarajan <aparnalovestocode@gmail.com>
2026-04-12 21:43:42 -0400

    Add: daily-temperatures - submission-1

---
+++ Data Structures & Algorithms/daily-temperatures/submission-1.py
@@ -0,0 +1,19 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        indexes = []
+        result = [0] * len(temperatures)
+
+        for idx, temp in enumerate(temperatures):
+            if len(indexes) == 0 or temperatures[indexes[len(indexes) - 1]] > temp:
+                indexes.append(idx)
+            else:
+                while len(indexes) > 0 and temperatures[indexes[len(indexes) - 1]] < temp:
+                    print(temperatures[indexes[len(indexes) - 1]])
+                    print(temp)
+                    removedIdx = indexes.pop()
+                    result[removedIdx] = idx - removedIdx
+                indexes.append(idx)
+
+        return result 
+
+
